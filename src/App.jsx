5380ae0a393b2816fe412d91cd4d4d2afbf7b716
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-28 15:29:21 -0300

    limpie el template

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>NOTICIAS</h1>
+      </header>
+      <main></main>
+      <footer className="text-center text-light bg-black py-1 sombra">
+        <p>
+          By{" "}
+          <a
+            href="https://github.com/KR3RULIE"
+            target="blank"
+            className="text-decoration-none text-danger"
+          >
+            KR3RULIE
+          </a>
+        </p>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
